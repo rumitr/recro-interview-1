36ec9121d86dac2c05b4b011f1ab807b98c0d5f6
rumit rout <rumit.rout@opsmx.io>
2021-06-15 20:04:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useCallback, useRef, useState } from "react";
+import "./App.css";
+import useAsync from "./hooks/useAsync";
+import limit from "./lib/constant";
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const { posts, loading, error } = useAsync(currentPage);
+
+  const observer = useRef();
+  const lastPostRef = useCallback(
+    (post) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setCurrentPage((prevPageNumber) => prevPageNumber + limit);
+        }
+      });
+      if (post && post.id < 99) observer.current.observe(post);
+    },
+    [loading]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {posts && (
+        <div className="post-grid">
+          {posts.map((item, index) => {
+            //add the ref to the last key, had to hardcode the last id
+            //because it keeps sending requests to the server
+            //can use a total lenght to track tje end of records to remove hardcoded values
+            if (posts.length === index + 1 && item.id < 99) {
+              return (
+                <div className="card" ref={lastPostRef} key={item}>
+                  <h2>{item.title}</h2>
+                  <p>{item.body}</p>
+                </div>
+              );
+            } else {
+              return (
+                <div className="card" key={item}>
+                  <h2>{item.title}</h2>
+                  <p>{item.body}</p>
+                </div>
+              );
+            }
+          })}
+        </div>
+      )}
+
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
+    </>
   );
 }
 
---
+++ src/hooks/useAsync.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import limit from "../lib/constant";
+
+function useAsync(currentPage) {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+
+    fetch(
+      `http://jsonplaceholder.typicode.com/posts?_start=${currentPage}&_limit=${limit}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts((prev) => [...prev, ...data]);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [currentPage]);
+  return { posts, loading, error };
+}
+
+export default useAsync;
